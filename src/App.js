9361c06ac43aeb7e758135bc5283b70f5670570e
Henry Wang <henrymw@vt.edu>
2019-08-15 21:59:32 -0400

    feat: v1, can expand to see followers and followers' followers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,272 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import * as _ from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import D3Graph from './D3Graph';
+
+const base = 'https://api.github.com'
+
+// const depth = 3;
+
+// const graph = {};
+
+const updateNodes = (existingNodes, newIDs) => {
+  console.log('newIDs', newIDs);
+  let newNodes = newIDs.map(githubID => ({id: githubID}));
+  console.log('New', newNodes);
+  return _.unionBy([...existingNodes], [...newNodes], 'id');
+};
+
+const updateLinks = (existingLinks, res) => {
+  let newLinks = res.reduce((acc, curr) => {
+    return acc.concat(curr.targets.map(targetID => {
+      return {source: curr.id, target: targetID}
+    }))
+  }, []);
+
+  // links could be duplicated
+  let allLinks = _.union(existingLinks, newLinks);
+
+  let uniqLinks = _.uniqWith(allLinks, (a, b) => {
+    return a.source === b.source && a.target === b.target
+  });
+
+  return uniqLinks;
+}
+
+const updateNextToExpand = (expanded, nextToExpand, newNodes) => {
+  // let nextNodes = updateNextToExpand(expanded, nextToExpand, nextIDs);
+  return _.difference(newNodes.map(d => d.id), nextToExpand, expanded);
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.state = {
+      searchResults: {},
+      width: 600,
+      height: 400,
+      id: 'barchart',
+      nodes: [], // {id: "abc"}, must be unique
+      links: [], // {source: "abc", target: "def"}
+      nextToExpand: [], //["henrymwang"] used to keep track of next nodes to expand
+      expanded: [], // ["henrymwang"] TODO: change to hashmap
+      depth: 0
+    };
+  }
+
+  async getUser(value) {
+    const resp = await fetch(`${base}/users/${value}`);
+    if (resp.status === 200) {
+      return await resp.json();
+    }
+    return Promise.reject(new Error(resp.status));
+  }
+
+  async getFollowers(user) {
+    const resp = await fetch(`${base}/users/${user}/followers`);
+    if (resp.status === 200) {
+      return await resp.json();
+    }
+    return Promise.reject(new Error(resp.status));
+  }
+
+  componentDidUpdate = async (prevProps, prevState) => {
+    if (prevState.depth !== this.state.depth) {
+      // update nodes
+      const { nextToExpand, nodes, expanded, links } = this.state;
+      console.log('Exp', expanded);
+
+      console.log('NextToExpand', nextToExpand);
+      // let nextIDs = nextToExpand.map(node => node.id);
+      let res = await Promise.all(nextToExpand.map(
+        async id => {
+          // could also do return this.getFollowers(id);
+          let jsonArr = await this.getFollowers(id);
+          let targets = jsonArr.map(json => json.login);
+          return {id: id, targets: targets};
+        }
+      ));
+
+      console.log('Res',res);
+
+      let newIDs = res.reduce((acc, curr) => {
+        return acc.concat(curr.targets);
+      }, []);
+      // console.log(newIDs);
+      let newNodes = updateNodes(nodes, newIDs);
+      console.log(newNodes);
+
+      let newLinks = updateLinks(links, res);
+
+      // bug: expanded will be out of date because this.getFollowers()
+      // updates this.state.expanded
+      let nextNodes = updateNextToExpand(expanded, nextToExpand, newNodes);
+
+      console.log('Next Nodes', nextNodes);
+      this.setState({nodes: newNodes, links: newLinks, nextToExpand: nextNodes});
+    }
+  }
+
+  handleIncrDepth() {
+    this.setState(({ depth }, props) => {
+      return {depth: depth + 1};
+    });
+  }
+
+  generateNodesLinks(res, githubID) {
+    const nodes = res.map(d => ({id: d.login}));
+    this.setState(prevState => ({ expanded: [...this.state.expanded, githubID] }));
+    this.setState({nextToExpand: [...res].map(d => d.login)});
+    const links = res.map(d => ({source: githubID, target: d.login}));
+    return [[...nodes], [...links]];
+  }
+
+  handleSearch() {
+    const value = this.textInput.current.value;
+    this.setState({depth: 0});
+    this.getUser(value)
+      .then(({login, followers, following, avatar_url}) => {
+        // destructure obj to access those fields
+        let searchResults = {
+          githubID: login,
+          followers: followers,
+          following: following,
+          avatarUrl: avatar_url
+        };
+        this.setState({searchResults: searchResults});
+        return searchResults.githubID;
+      })
+      .catch(err => {
+        // TODO: display user not found
+        this.setState({searchResults: {}});
+        console.log(err);
+      })
+      .then(githubID => {
+        this.getFollowers(githubID)
+          .then(res => {
+            const [nodes, links] = this.generateNodesLinks(res, githubID);
+            console.log(nodes);
+            let allNodes = [...nodes];
+            allNodes.unshift({id: githubID}); // add myself to front
+            this.setState({nodes: allNodes, links: links});
+          })
+          .catch(err => {
+            // TODO: display user not found
+            this.setState({searchResults: {}});
+            console.log(err);
+          });
+      })
+      ;
+  }
+
+  render() {
+    let { searchResults } = this.state;
+    let githubID = searchResults.githubID;
+    let followers = searchResults.followers;
+    let following = searchResults.following;
+    let avatarUrl = searchResults.avatarUrl;
+
+    let rows;
+    if (Object.keys(searchResults).length === 0) {
+      rows = (<tr></tr>)
+    } else {
+      rows = (
+        <tr>
+          <td>
+            <img alt={"avatar of " + githubID} width={50} height={50} src={avatarUrl}>
+          </img>
+          </td>
+          <td>{ githubID }</td>
+          <td>{ followers }</td>
+          <td>{ following }</td>
+        </tr>
+      );
+    }
+
+    return (
+      <div className="App">
+      <Container>
+        <Row>
+          <Col className="d-none d-lg-block"></Col>
+          <Col>
+            <h3>
+              Github Network
+            </h3>
+            <div>
+              <InputGroup className="mb-3">
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  placeholder="Username"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                  ref={this.textInput}
+                  type="text"
+                />
+                <InputGroup.Append>
+                  <Button variant="outline-secondary" onClick={_ => this.handleSearch()}>Search</Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </div>
+          </Col>
+          <Col className="d-none d-lg-block"></Col>
+        </Row>
+        <Row>
+          <Col>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Avatar</th>
+                  <th>Username</th>
+                  <th>Followers</th>
+                  <th>Following</th>
+                </tr>
+              </thead>
+              <tbody>
+                { rows }
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+        <Row>
+          <D3Graph nodes={this.state.nodes} links={this.state.links}>
+          </D3Graph>
+        </Row>
+        <Row>
+          <Col>
+          {
+             (this.state.nodes.length > 0 && this.state.depth < 3) && (
+               <div>
+                <Button variant="outline-secondary" onClick={_ => this.handleIncrDepth()}>
+                  Increase Depth
+                </Button>
+                <p>Depth: {this.state.depth}</p>
+              </div>
+            )
+          }
+
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <div id="svg-container">
+        </div>
+      </Container>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/D3Graph.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+import * as _ from 'lodash';
+import { schemeCategory10 } from 'd3-scale-chromatic';
+
+
+class D3Graph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      svg: ''
+    }
+  }
+  // first time
+  componentDidMount() {
+    console.log(this.props.nodes);
+    console.log(this.props.links);
+    this.drawChart();
+  }
+  // update
+  componentDidUpdate(prevProps, prevState) {
+    if (!_.isEqual(prevProps.nodes, this.props.nodes) || !_.isEqual(prevProps.links, this.props.links)) {
+      this.clear();
+      this.drawChart();
+    }
+  }
+
+  clear() {
+    if (this.state.svg) {
+      this.state.svg.remove();
+    }
+  }
+
+  drawChart(c) {
+    const w = 600;
+    const h = 400;
+    const color = schemeCategory10;
+    const svg = d3.select('#svg-container')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h);
+    // var simulation = d3.forceSimulation()
+    //     .force("x", d3.forceX(w/2).strength(0.4))
+    //     .force("y", d3.forceY(h/2).strength(0.6))
+    //     .force("link", d3.forceLink().id((d) => d.id)) // specify id is the link variable
+    //     .force("charge", d3.forceManyBody()) // // nodes repel from each other, preventing overlap
+    //     .force("center", d3.forceCenter(w / 2, h / 2)); // pulls nodes to center
+
+    var simulation = d3.forceSimulation()
+        .force("x",d3.forceX(w/2).strength(0.4))
+        .force("y",d3.forceY(h/2).strength(0.6))
+        .force("charge",d3.forceManyBody().strength(-1000))
+        .force("link", d3.forceLink().id(d =>  d.id ))
+        .force("collide",d3.forceCollide().radius(d => d.r * 10))
+    var link = svg.append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(this.props.links)
+      .enter().append("line")
+        .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+    var node = svg.append("g")
+        .attr("class", "nodes")
+      .selectAll("g")
+      .data(this.props.nodes)
+      .enter().append("g")
+
+    var circles = node.append("circle")
+        .attr("r", 5)
+        .attr("fill", function(d) { return 'green';/*color(d.group);*/ })
+        // .call(d3.drag()
+        //     .on("start", dragstarted)
+        //     .on("drag", dragged)
+        //     .on("end", dragended));
+
+    var labels = node.append('text')
+      .text(d => d.id)
+      .attr('x', 6)
+      .attr('y', 3);
+
+    const ticked = () => {
+      link
+          .attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+
+      node
+          .attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+          })
+    };
+
+    simulation
+        .nodes(this.props.nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(this.props.links);
+
+    simulation.on('tick', ticked);
+    simulation.alpha(1).restart();
+
+    // add svg to state
+    this.setState({svg: svg});
+  }
+
+  render() {
+    return (
+      <div id={'#something'}>
+      </div>
+    );
+  }
+}
+
+export default D3Graph;
